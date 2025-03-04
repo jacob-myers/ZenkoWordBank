e1f160798e8b4b901fd162e241a3ea410b08b6c6
jacob-myers <jakemyers190@gmail.com>
2025-03-04 15:43:14 -0500

    Cleaning, tweaking and finalizing home page.

--- lib/widgets/pages/page_home.dart
+++ lib/widgets/pages/page_home.dart
@@ -28,10 +28,10 @@ class _PageHome extends State<PageHome> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         Container(
-          padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-          child: Text(
+          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+          child: const Text(
               "Word of the day",
             style: JWBTextStyles.headerText,
           ),
@@ -39,97 +39,95 @@ class _PageHome extends State<PageHome> {
         SizedBox(
           height: 270,
           child: FutureBuilder(
-              future: DictDatabaseHelper.instance.getRandomWord(DateTime.now().millisecondsSinceEpoch ~/ (1000 * 60 * 60 * 24)),
-              builder: (BuildContext context, AsyncSnapshot<TermEntry> snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(child: Text("Loading..."));
-                }
-                //dailyTerm = snapshot.data;
-                return Column(
-                  children: [
-                    TermCard(term: snapshot.data!),
-                    SizedBox(height: 3),
-                    TextButton(
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return  Dialog.fullscreen(
-                              child: TermEditor(
-                                term: snapshot.data,
-                                onClose: () {
-                                  setState(() {});
-                                }
-                              ),
-                            );
-                          }
-                        );
-                      },
-                      style: ButtonStyle(
-                        textStyle: WidgetStatePropertyAll(JWBTextStyles.newTermButton),
-                        backgroundColor: WidgetStatePropertyAll(JWBColors.newTermButtonConfirm),
-                        foregroundColor: WidgetStatePropertyAll(JWBColors.entryTextMain),
-                      ),
-                      child: Text( "Add to bank" ),
-                    ),
-                    //Spacer()
-                  ],
-                );
+            future: DictDatabaseHelper.instance.getRandomWord(DateTime.now().millisecondsSinceEpoch ~/ (1000 * 60 * 60 * 24)),
+            builder: (BuildContext context, AsyncSnapshot<TermEntry> snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: Text("Loading..."));
               }
+              return Column(
+                children: [
+                  TermCard(term: snapshot.data!),
+                  const SizedBox(height: 3),
+                  TextButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return  Dialog.fullscreen(
+                            child: TermEditor(
+                              term: snapshot.data,
+                              onClose: () {
+                                setState(() {});
+                              }
+                            ),
+                          );
+                        }
+                      );
+                    },
+                    style: const ButtonStyle(
+                      textStyle: WidgetStatePropertyAll(JWBTextStyles.newTermButton),
+                      backgroundColor: WidgetStatePropertyAll(JWBColors.newTermButtonConfirm),
+                      foregroundColor: WidgetStatePropertyAll(JWBColors.entryTextMain),
+                    ),
+                    child: Text( "Add to bank" ),
+                  ),
+                ],
+              );
+            }
           ),
         ),
 
         Card(
-          margin: EdgeInsets.fromLTRB(5, 5, 70, 2),
+          margin: const EdgeInsets.fromLTRB(5, 5, 70, 2),
           child: InkWell(
             onTap: () { widget.navigateToPageN != null ? widget.navigateToPageN!(0) : null; },
             child: Container(
-                padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                decoration: BoxDecoration(
-                  color: JWBColors.entryBackground,
-                  borderRadius: BorderRadius.circular(8),
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+              decoration: BoxDecoration(
+                color: JWBColors.entryBackground,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const SizedBox(
+                height: 50,
+                child: Row(
+                  children: [
+                    Icon(Icons.arrow_back_ios_new),
+                    SizedBox(width: 10),
+                    Text(
+                      "Word Bank",
+                      style: JWBTextStyles.homeCardText,
+                    ),
+                  ],
                 ),
-                child: SizedBox(
-                  height: 50,
-                  child: Row(
-                    children: [
-                      Icon(Icons.arrow_back_ios_new),
-                      SizedBox(width: 10),
-                      Text(
-                        "Word Bank",
-                        style: JWBTextStyles.homeCardText,
-                      ),
-                    ],
-                  ),
-                )
+              )
             ),
           ),
         ),
 
         Card(
-          margin: EdgeInsets.fromLTRB(70, 5, 5, 2),
+          margin: const EdgeInsets.fromLTRB(70, 5, 5, 2),
           child: InkWell(
             onTap: () { widget.navigateToPageN != null ? widget.navigateToPageN!(2) : null; },
             child: Container(
-                padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                decoration: BoxDecoration(
-                  color: JWBColors.entryBackground,
-                  borderRadius: BorderRadius.circular(8),
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+              decoration: BoxDecoration(
+                color: JWBColors.entryBackground,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const SizedBox(
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Translator",
+                      style: JWBTextStyles.homeCardText,
+                    ),
+                    SizedBox(width: 10),
+                    Icon(Icons.arrow_forward_ios),
+                  ],
                 ),
-                child: SizedBox(
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Translator",
-                        style: JWBTextStyles.homeCardText,
-                      ),
-                      SizedBox(width: 10),
-                      Icon(Icons.arrow_forward_ios),
-                    ],
-                  ),
-                )
+              )
             ),
           ),
         ),
